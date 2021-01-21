311331566b92dea756b2a1c8d2ffd554e8310da6
Davi Almeida <davicastroti@hotmail.com>
2021-01-21 01:53:12 -0300

    Uma pequena apresentação ao usuario

--- billy.py
+++ billy.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+print("="*50)
+print('''Bem vindo ao Billy \nDesenvolvido por Davi "yotta" Almeida ''')
+print("="*50)
 driver = webdriver.Chrome(executable_path=r"C:\Program Files\Dev\chromedriver.exe")
 
 driver.get("https://google.com.br")
