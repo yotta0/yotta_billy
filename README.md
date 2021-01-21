45f3b5fd66750b8c7e43b2bbe8ebeff36c2dd8db
Davi Almeida <davicastroti@hotmail.com>
2021-01-21 20:08:31 -0300

    Apenas algumas atualizações no raciocinio

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # yotta_billy
- A simple but useful selenium bot.
- projeto de automação de testes em navegadores 
+ - A simple but useful selenium bot.
+ - projeto de automação de testes em navegadores 
 
- Tecnologia Usadas: Python, Selenium 
+ - Tecnologia Usadas: Python, Selenium 
 
- Ideia Central :
- Criar um Script que automatize testes em navegador e adapta-lo para fazer o trabalho por você :)
- O intuito principal de tudo é aprender.
+ - Ideia Central 
+ - Criar um Script que automatize testes em navegador e adapta-lo para fazer o trabalho por você :)
+ - O intuito principal de tudo é aprender.
--- billy.py
+++ billy.py
@@ -2,16 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-driver = webdriver.Chrome(executable_path=r"C:\Program Files\Dev\chromedriver.exe")
 print("="*50)
 print('''Bem vindo ao Billy \nDesenvolvido por Davi "yotta" Almeida ''')
 print("="*50)
+driver = webdriver.Chrome(executable_path=r"C:\Program Files\Dev\chromedriver.exe")
 
-def login():
-    name_login = "uabb-lf-name"
-    passw_login = ""
-
-
+name_login = "uabb-lf-name"
+passw_login = "uabb-lf-password"
+name_user = "davicastroti@hotmail.com"
+passw_user = "Jdac455247rd1414."
 
 driver.get("https://www.cursoemvideo.com/login/")
+
+element_name = driver.find_element_by_name(name_login)
+element_passw = driver.find_element_by_name(passw_user)
+
+element_name.send_keys(name_user)
+
+
+
+
+
+
 
