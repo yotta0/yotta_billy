314ea906bb4139003174b512059185f732be6f67
Davi Almeida <davicastroti@hotmail.com>
2021-01-21 19:24:11 -0300

    Update billy.py

--- billy.py
+++ billy.py
@@ -2,17 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+driver = webdriver.Chrome(executable_path=r"C:\Program Files\Dev\chromedriver.exe")
 print("="*50)
 print('''Bem vindo ao Billy \nDesenvolvido por Davi "yotta" Almeida ''')
 print("="*50)
 
-driver = webdriver.Chrome(executable_path=r"C:\Program Files\Dev\chromedriver.exe")
+def login():
+    name_login = "uabb-lf-name"
+    passw_login = ""
 
-driver.get("https://google.com.br")
 
-elem = driver.find_element_by_name("q")
-elem.click()
-elem.send_keys("Curso em Video" + Keys.RETURN)
 
-elem = driver.find_element_by_css_selector("h3.LC20lb DKV0Md")
-elem.click()
+driver.get("https://www.cursoemvideo.com/login/")
+
